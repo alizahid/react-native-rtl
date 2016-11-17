cfb23bb85895fdc452763992c2621da59f29d6be
Ali Zahid <ali.zahid@live.com>
2016-11-17 14:01:12 +0400

    Refactoring

--- index.ios.js
+++ index.ios.js
@@ -1,169 +1,146 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, {
-	Component,
-} from 'react'
+import React, {Component} from 'react'
 
 import {
-	AppRegistry,
-	I18nManager,
-	ListView,
-	View,
-	StyleSheet,
-	Switch,
-	Text,
-	TextInput,
+    AppRegistry,
+    I18nManager,
+    ListView,
+    ScrollView,
+    StyleSheet,
+    Switch,
+    Text,
+    TextInput,
+    View
 } from 'react-native'
 
 import InvertibleScrollView from 'react-native-invertible-scroll-view'
 
 export default class RTLapp extends Component {
-	constructor() {
-		super()
+    constructor() {
+        super()
 
-		const ds = new ListView.DataSource({
-			rowHasChanged: (r1,
-			r2) => r1 !== r2,
-		});
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        })
 
-		this.state = {
-			dataSource: ds.cloneWithRows([
-				'1 John',
-				'2 Joel',
-				'3 James',
-				'4 Jimmy',
-				'5 Jackson',
-				'6 Jillian',
-				'7 Julie',
-				'8 Devin',
-			]),
-			isRTL: I18nManager.isRTL,
-		}
-	}
+        this.state = {
+            dataSource: ds.cloneWithRows([
+                '1 John',
+                '2 Joel',
+                '3 James',
+                '4 Jimmy',
+                '5 Jackson',
+                '6 Jillian',
+                '7 Julie',
+                '8 Devin'
+            ]),
+            isRTL: I18nManager.isRTL
+        }
+    }
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<View style={styles.things}>
-					<Text style={[
-						styles.thing,
-						styles.red,
-					]}>1</Text>
-					<Text style={[
-						styles.thing,
-						styles.blue,
-					]}>2</Text>
-					<Text style={[
-						styles.thing,
-						styles.green,
-					]}>3</Text>
-				</View>
+    _onDirectionChange() {
+        I18nManager.forceRTL(!this.state.isRTL)
 
-				<View style={styles.separator}/>
+        this.setState({
+            isRTL: !this.state.isRTL
+        })
+    }
 
-				<TextInput style={styles.input} placeholder="Hello"/>
+    render() {
+        return (
+            <ScrollView style={styles.root} contentContainerStyle={styles.container}>
+                <Switch onValueChange={this._onDirectionChange.bind(this)} value={this.state.isRTL}/>
 
-				<View style={styles.separator}/>
+                <View style={styles.separator}/>
 
-				<ListView
-					style={styles.list}
-					renderScrollComponent={props => <InvertibleScrollView {...props} />}
-					contentContainerStyle={styles.listContainer}
-					dataSource={this.state.dataSource}
-					renderRow={rowData => <Text style={styles.listItem}>{rowData}</Text>}
-					horizontal={true}
-				/>
+                <View style={styles.things}>
+                    <Text style={[styles.thing, styles.red]}>1</Text>
+                    <Text style={[styles.thing, styles.blue]}>2</Text>
+                    <Text style={[styles.thing, styles.green]}>3</Text>
+                </View>
 
-				<View style={styles.separator}/>
+                <View style={styles.separator}/>
 
-				<ListView
-					style={styles.list}
-					renderScrollComponent={props => <InvertibleScrollView {...props} inverted />}
-					contentContainerStyle={styles.listContainer}
-					dataSource={this.state.dataSource}
-					renderRow={rowData => <Text style={styles.listItem}>{rowData}</Text>}
-					horizontal={true}
-				/>
-				
-				<View style={styles.separator}/>
+                <TextInput style={styles.input} placeholder="Hello"/>
 
-				<Text style={styles.arabic}>أدخل عنوان البريد الإلكتروني الذي استخدمته لإنشاء حساب جادو بادو الخاص بك وسوف نرسل لك رسالة بريد إلكتروني لإعادة تعيين كلمة السر</Text>
+                <View style={styles.separator}/>
 
-				<View style={styles.separator}/>
+                <ListView style={styles.list} renderScrollComponent={props => <InvertibleScrollView {...props}/>} contentContainerStyle={styles.listContainer} dataSource={this.state.dataSource} renderRow={this._renderRow} horizontal={true}/>
 
-				<Switch onValueChange={this._onDirectionChange.bind(this)} style={styles.rightAlignStyle} value={this.state.isRTL}/>
-			</View>
-		)
-	}
+                <View style={styles.separator}/>
 
-	_onDirectionChange() {
-		I18nManager.forceRTL(!this.state.isRTL)
-		this.setState({
-			isRTL: !this.state.isRTL,
-		})
+                <ListView style={styles.list} renderScrollComponent={props => <InvertibleScrollView {...props} inverted/>} contentContainerStyle={styles.listContainer} dataSource={this.state.dataSource} renderRow={this._renderRow} horizontal={true}/>
 
-		this.forceUpdate()
-	}
+                <View style={styles.separator}/>
+
+                <Text style={styles.arabic}>أدخل عنوان البريد الإلكتروني الذي استخدمته لإنشاء حساب جادو بادو الخاص بك وسوف نرسل لك رسالة بريد إلكتروني لإعادة تعيين كلمة السر</Text>
+            </ScrollView>
+        )
+    }
+
+    _renderRow(text, section, row) {
+        let style = [
+            styles.listItem, row % 2 === 0 && styles.even
+        ]
+
+        return <Text style={style}>{text}</Text>
+    }
 }
 
 const styles = StyleSheet.create({
-	blue: {
-		backgroundColor: 'blue',
-	},
-	container: {
-		alignItems: 'center',
-		backgroundColor: '#EEE',
-		flex: 1,
-		justifyContent: 'center',
-		padding: 20,
-		paddingTop: 40
-	},
-	green: {
-		backgroundColor: 'green',
-	},
-	list: {
-		backgroundColor: 'white',
-		flexGrow: 0,
-	},
-	listContainer: {
-		alignItems: 'flex-start'
-		// flexDirection: 'row-reverse',
-		// transform: [
-	    //   { scaleX: -1 },
-	    // ]
-	},
-	listItem: {
-		padding: 20,
-		// alignSelf: 'flex-end'
-	},
-	input: {
-		backgroundColor: 'white',
-		height: 60,
-		padding: 20,
-		textAlign: I18nManager.isRTL ? 'right' : 'left',
-	},
-	red: {
-		backgroundColor: 'red',
-	},
-	separator: {
-		marginBottom: 10,
-		marginTop: 10,
-	},
-	thing: {
-		color: 'white',
-		padding: 20,
-	},
-	things: {
-		flexDirection: 'row',
-	},
-	arabic: {
-		// alignSelf: 'flex-start',
-		textAlign: 'right'
-	}
+    root: {
+        backgroundColor: '#EEE',
+        flex: 1,
+        padding: 20,
+        paddingTop: 40
+    },
+    container: {
+        alignItems: 'stretch'
+    },
+    things: {
+        flexDirection: 'row'
+    },
+    thing: {
+        color: 'white',
+        flex: 1,
+        paddingVertical: 20,
+        textAlign: 'center'
+    },
+    red: {
+        backgroundColor: 'red'
+    },
+    blue: {
+        backgroundColor: 'blue'
+    },
+    green: {
+        backgroundColor: 'green'
+    },
+    input: {
+        backgroundColor: 'white',
+        height: 60,
+        padding: 20,
+        textAlign: I18nManager.isRTL ? 'right' : 'left'
+    },
+    list: {
+        backgroundColor: 'white'
+    },
+    listContainer: {
+        alignItems: 'flex-start'
+    },
+    listItem: {
+        padding: 20
+    },
+    even: {
+        backgroundColor: '#CCC'
+    },
+    arabic: {
+        backgroundColor: 'white',
+        padding: 20,
+        textAlign: 'right'
+    },
+    separator: {
+        marginBottom: 10,
+        marginTop: 10
+    }
 })
 
 AppRegistry.registerComponent('RTLapp', () => RTLapp)
